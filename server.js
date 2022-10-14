e93adc3ba501948e35a6113c5bec357b9377a5d6
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-14 15:28:00 +0100

    Using the util unction for message formatting
    setting an admin name, importing the util/helper function with which to format messages, then format the message payload where applicable

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express =  require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+//import the utility function
+const formatMessage = require('./utils/messages');
 
 const app = express();
 //creating a server instance
@@ -12,17 +14,19 @@ const io = socketio(server);
 //set static folder (public folder)
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Raptor'
+
 //When a client connects
 io.on('connection', (socket) => {
     console.log(`${socket.id} is a new connection....`)
     //send welcome message to client
-    socket.emit('message', 'Welcome to chart room')
+    socket.emit('message', formatMessage(botName, 'Welcome to chart room'))
     //notify others when a user joins their room
-    socket.broadcast.emit('message', 'A user joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user joined the chat'));
     //also notify when a client disconnects
     socket.on('disconnect', () => {
         //notify everyone about the user that left
-        io.emit('message', 'A user left the chat');
+        io.emit('message', formatMessage(botName, 'A user left the chat'));
     });
     //listen for chatMessage from client
     socket.on('chatMessage', (clientmsg) => {
