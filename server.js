7b554e17c892544844d21aaf58071ff9e104c7a8
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-14 11:18:34 +0100

    Server setup
    Setup the boilerplate for the server.js (server file), and set path to static folder: public(html, css, javascript)

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express =  require('express');
+const path = require('path');
 
 const app = express();
+//set static folder (public folder)
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
