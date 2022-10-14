bc3834df0944cd770c415d3af622b18d05db3841
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-14 17:53:44 +0100

    Utility function update
    Functions to manage user exiting the room, and getting list of users in a room

--- utils/users.js
+++ utils/users.js
@@ -16,7 +16,22 @@ function getCurrentUser(id) {
     return users.find((user) => user.id === id)
 }
 
+//User exiting the chat
+function userLeaves(id) {
+    const index = users.findIndex((user) => user.id === id);
+    if(index !== -1) {
+        //return the user that's exiting
+        return users.splice(index, 1)[0];
+    }
+}
+
+//get users in a room
+function usersInRoom(room){
+    //return users with room property = to the room param passed
+    return users.filter((user) => user.room === room);
+}
+
 //export for use
 module.exports = {
-    addUser, getCurrentUser
+    addUser, getCurrentUser, userLeaves, usersInRoom
 };
